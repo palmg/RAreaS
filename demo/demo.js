68f5ac709d051afb28088fc312494d2401c26d35
chkui <chenkui@chkui.com>
2017-07-07 23:08:23 +0800

    add project

---
+++ demo/demo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chkui on 2017/7/7.
+ */
+
+import React from 'react'
+import {render} from 'react-dom'
+
+import RAreas from '../lib/index'
+import './demo.scss'
+
+render(<RAreas />, document.getElementById('root'))
+
