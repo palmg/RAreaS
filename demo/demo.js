2868fa54bb10129521a1f5c02d930570f7459bbb
chkui <chenkui@chkui.com>
2017-07-10 14:57:59 +0800

    complete dev

--- demo/demo.js
+++ demo/demo.js
@@ -5,8 +5,51 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import RAreas from '../lib/index'
+import RAreas from '../index'
 import './demo.scss'
 
-render(<RAreas />, document.getElementById('root'))
+class Demo extends React.Component{
+    constructor(...props){
+        super(...props)
+        this.state = {
+            show:false,
+            area:''
+        }
+        this.popHandle = this.popHandle.bind(this)
+        this.cancelHandle = this.cancelHandle.bind(this)
+        this.commitHandle = this.commitHandle.bind(this)
+    }
+
+    popHandle(){
+        this.setState({
+            show:true
+        })
+    }
+
+    cancelHandle(){
+        this.setState({
+            show:false
+        })
+    }
+
+    commitHandle(data){
+        this.setState({
+            show:false,
+            area:`${data.province.name}${data.city.name}${data.area.name}`
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.popHandle}>地区选择</button>
+                <p>选择结果：</p>
+                <p>{this.state.area}</p>
+                {this.state.show && (<RAreas onCancel={this.cancelHandle} onCommit={this.commitHandle}/>)}
+            </div>
+        )
+    }
+}
+
+render(<Demo />, document.getElementById('root'))
 
---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by chkui on 2017/7/7.
+ */
+module.exports = require('./lib/areas').default
+module.exports.default = module.exports
